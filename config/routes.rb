e860af913654c2319bbe062aa4dd27ef8f4b067e
JohnDGreiner <john.d.greiner@gmail.com>
2015-06-05 09:28:24 -0400

    Update views and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   resources :exercise_tasks
   resources :weights
   resources :cals
-  root 'dashboard#index'
+
+  root 'weights#dashboard'
+  get 'weights/dashboard'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
