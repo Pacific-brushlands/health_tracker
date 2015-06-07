be71312d9286a63e8b6537e418f5e2cbc2a25fe7
Aaron Wiggins <apw7177@gmail.com>
2015-06-07 13:20:03 -0400

    no faker in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ foods = ["Apple Pie", "Sweet & Sour Chicken", "Breakfast Sandwich", "Tuna Casser
     "Pop Tarts", "Cheddar Biscuits", "Steak", "Brownies", "Hash Browns", "Bacon",
     "Pear", "Green Beans", "Mac & Cheese", "Fried Chicken", "Grapes"]
 
-
+random_number = (100..999).to_a.sample
 
 count = 0
 4.times do
@@ -23,7 +23,7 @@ count = 0
 4.times do
   ExerciseTask.create!(
     exercise_type_id: count,
-    cals_burned: Faker::Number.number(3).to_i,
+    cals_burned: random_number,
     exercise_on: Date.today-count
     )
   count += 1
@@ -31,7 +31,7 @@ end
 
 20.times do
   Cal.create!(
-    amount: Faker::Number.number(3).to_i,
+    amount: random_number,
     description: foods.sample,
     cals_on: Date.today-(0..4).to_a.sample
   )
@@ -40,7 +40,7 @@ end
 count = 0
 4.times do
   Step.create!(
-    amount: Faker::Number.number(3).to_i,
+    amount: random_number,
     steps_on: Date.today.day-count
   )
   count += 1
